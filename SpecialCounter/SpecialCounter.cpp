5ce3e812b1446ed8e1259ef71e4149fd3aad9103
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-03-13 11:38:52 +0200

    Improve start() and stop methods() Add increment and decrement overrides

--- SpecialCounter/SpecialCounter.cpp
+++ SpecialCounter/SpecialCounter.cpp
@@ -12,11 +12,13 @@ SpecialCounter::~SpecialCounter()
 
 void SpecialCounter::start(int start)
 {
+	if ((start >= 0) && (start <= stopVal))
 	startVal = start;
 }
 
 void SpecialCounter::stop(int stop)
 {
+	if ((stop <= 255) && (stop >= startVal))
 	stopVal = stop;
 }
 
@@ -29,5 +31,33 @@ void SpecialCounter::stepDec(int stepSize)
 {
 	decVal = stepSize;
 }
+
+SpecialCounter SpecialCounter::operator++() 
+{
+	count += 1;
+	return *this;    
+}
+
+SpecialCounter SpecialCounter::operator--() 
+{
+	count -= 1;
+	return *this;
+}
+
+SpecialCounter SpecialCounter::operator++(int)
+{
+	SpecialCounter duplicate(*this);
+	count += 1;
+	return duplicate;              
+}
+
+SpecialCounter SpecialCounter::operator--(int)
+{
+	SpecialCounter duplicate(*this);
+	count -= 1;
+	return duplicate;
+}
+
+
 
 
--- SpecialCounter/SpecialCounter.h
+++ SpecialCounter/SpecialCounter.h
@@ -3,6 +3,7 @@ class SpecialCounter
 {
 private:
 	int startVal = 0;
+	int count = startVal;
 	int stopVal = 255;
 	int incVal = 1;
 	int decVal = 1;
@@ -14,5 +15,11 @@ public:
 	void stop(int stop);
 	void stepInc(int stepSize);
 	void stepDec(int stepSize);
+	void counter();
+	SpecialCounter operator++();
+	SpecialCounter operator--();
+	SpecialCounter operator++(int num);
+	SpecialCounter operator--(int num);
+
 };
 
